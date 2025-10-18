d01c9334b4ddab41e7870d63128906b7305c5a7a
AdityaRaj019 <adityaraj9341376019@gmail.com>
2025-10-18 17:14:45 +0530

    Feat: Add header and dropdown menu

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+
+
+const page = () => {
+  return (
+    // <div className="min-h-screen home-wrapper p-6 md:p-8 lg:p-10"></div>
+    <div className="min-h-screen home-wrapper">
+      Home
+    </div>
+  )
+}
+
+export default page
