268c380010cfa18f16a394cd8ec190434debc3d5
AdityaRaj019 <adityaraj9341376019@gmail.com>
2025-10-19 11:57:03 +0530

    Feat:implement home page with widget

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import UserDropdown from "./UserDropdown";
 const Header = () => {
   return (
     <header className="sticky top-0 header">
-        <div className="cointainer header-wrapper">
+        <div className="container header-wrapper">
             <Link href='/'>
                 <Image src ="/assets/icons/logo.svg" alt="signalist-logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
                 
